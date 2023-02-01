f73030ee2c1f18239cad162ca3b9ee6e77bff7c3
spiritoftime <chooliangyi6@gmail.com>
2023-02-01 19:10:13 +0800

    fix language display at individualpage

--- src/pages/Individual Page/Detail.jsx
+++ src/pages/Individual Page/Detail.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Detail = ({ title, contentType = "", content }) => {
-  let languageNames = new Intl.DisplayNames(["en"], { type: "language" });
+  let languageNames =
+    contentType === "language"
+      ? new Intl.DisplayNames([content], { type: "language" })
+      : "";
+  console.log(languageNames);
   return (
     <div className="flex text-center flex-col gap-3">
       <p className="text-gray-500 font-normal">{title}</p>
       <p className="text-white  font-bold">
-        {contentType === "language" ? languageNames.of("en") : content}
+        {contentType === "language" ? languageNames.of(content) : content}
       </p>
     </div>
   );
--- src/pages/Individual Page/Stars.jsx
+++ src/pages/Individual Page/Stars.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import classes from "./stars.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
